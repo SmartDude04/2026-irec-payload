bae8f44e7b27e199662b55d5222f288bf7449114
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-03-05 13:02:47 -0500

    Add pressure abstraction layer

---
+++ lib/pressure/pressure.h
@@ -0,0 +1,46 @@
+#ifndef INC_2026_IREC_PAYLOAD_PRESSURE_H
+#define INC_2026_IREC_PAYLOAD_PRESSURE_H
+
+#include <Adafruit_MPRLS.h>
+
+class pressure
+{
+public:
+    pressure();
+
+    /**
+     * Attempts to find and initialize the MPRLS Pressure Sensor at I2C address 0x18 with pinouts:
+     * - 3.3V -> VIN
+     * - GND ->  GND
+     * - SDA ->  SDA
+     * - SCL ->  SCL
+     * - EOC ->  7
+     * - RST ->  8
+     * @return True if successfully found the sensor and initialized it; false otherwise
+     */
+    bool init();
+
+    /**
+     * Performs a hardware reset of the pressure sensor
+     */
+    static void reset();
+
+    /**
+     * Returns the pressure in hectopascals (1 hPa = 100 Pa)
+     * @return Pressure in hectopascals or NAN on failure
+     */
+    float get_pressure_hpa();
+
+    /**
+     * Returns the pressure in PSI
+     * @return Pressure in PSI or NAN on failure
+     */
+    float get_pressure_psi();
+    
+    
+private:
+    Adafruit_MPRLS mprls;
+};
+
+
+#endif
