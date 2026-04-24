5d5272ee61ae727dd818f5ef4c53ab561ac1154d
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2026-04-23 21:41:40 -0400

    Add altitude global variable and add debugging values for thresholds

--- include/rocket.h
+++ include/rocket.h
@@ -10,7 +10,8 @@
 #include <Servo.h>
 
 constexpr bool DEBUG = true;
-constexpr double EXPERIMENT_ACCEL_THRESHOLD = 0.1;
+constexpr double EXPERIMENT_ACCEL_THRESHOLD = DEBUG ? 5.0 : 0.5;
+constexpr double EXPERIMENT_ALTITUDE_THRESHOLD = DEBUG ? 0.0 : 500.0;
 constexpr double ACCELERATION_GROUND = 9.8;
 constexpr unsigned long RADIO_WAIT_TIME = 100;
 
@@ -23,12 +24,13 @@ extern accelerometer_t accelerometer;
 extern altimeter_t altimeter;
 
 extern double acceleration_norm;
+extern double altitude_m;
 extern bool experiment_started;
 extern bool armed;
 extern uint8_t radio_packet[protocol_t::MIN_BYTES_FOR_PACKET];
 
 // TODO: Add documentation
-void avg_acceleration();
+void update_acceleration_altitude();
 void handle_incoming_radio_messages();
 bool send_arm_ack(bool arm_state);
 void send_environmental_packet();
